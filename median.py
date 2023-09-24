b95599bd9a548e5981f09bb4e34cead37649836f
Johnathan Dawber <johnathan.dawber@kcl.ac.uk>
2023-09-24 10:12:13 -0700

    first solution

--- median.py
+++ median.py
@@ -1,6 +1,16 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def middle(list):
+     mid = len(list)
+     if mid%2 == 1:
+         return list[mid//2]
+     else:
+         average = (list[mid//2]+list[(mid-1)//2])/2
+         return average
+
+    
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +19,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(middle(numbers))
